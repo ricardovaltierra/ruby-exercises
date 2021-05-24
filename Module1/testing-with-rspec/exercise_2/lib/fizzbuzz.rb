337f2070754eed65a7a0b63c765843315a2225e9
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-05-24 12:59:10 -0500

    RUBY DOJO - Module 1 - Testing with RSpec exercise_2 (Fix done)

--- Module1/testing-with-rspec/exercise_2/lib/fizzbuzz.rb
+++ Module1/testing-with-rspec/exercise_2/lib/fizzbuzz.rb
@@ -1,14 +1,10 @@
 class FizzBuzz
   def self.play(number)
-    
-    if number % 3 == 0 && number % 5 == 0
-      return "FizzBuzz"
-    elsif (number % 3 == 0) || (number.to_s.split('').any? '3')
-      return "Fizz"
-    elsif (number % 5 == 0) || (number.to_s.split('').any? '3')
-      return "Buzz"
-    else 
-      return number
-    end
+    result_word = ''
+
+    result_word += 'Fizz' if number % 3 == 0 || (number.to_s.split('').any? '3')
+    result_word += 'Buzz' if number % 5 == 0 || (number.to_s.split('').any? '5')
+
+    result_word
   end
 end
--- Module1/testing-with-rspec/exercise_2/spec/fizzbuzz_spec.rb
+++ Module1/testing-with-rspec/exercise_2/spec/fizzbuzz_spec.rb
@@ -7,19 +7,19 @@ describe FizzBuzz do
 
     # Step 1
 
-    it "Numbers divisible by 3, the game says 'Fizz'" do
+    it "Numbers 3 is divisible by 3, the game says 'Fizz'" do
       result = FizzBuzz.play(3)
 
       expect(result).to eql 'Fizz'
     end
 
-    it "Numbers divisible by 5, the game says 'Buzz'" do
+    it "Numbers 5 is divisible by 5, the game says 'Buzz'" do
       result = FizzBuzz.play(5)
 
       expect(result).to eql 'Buzz'
     end
 
-    it "Numbers divisible by 3 **and** 5, the game says 'FizzBuzz'" do
+    it "Number 15 is divisible by 3 **and** 5, the game says 'FizzBuzz'" do
       result = FizzBuzz.play(15)
 
       expect(result).to eql 'FizzBuzz'
@@ -27,19 +27,23 @@ describe FizzBuzz do
 
     # Step 2
     
-    it "Number has a 3 in it, the game says 'Fizz'" do
-      result = FizzBuzz.play(1_015_423)
+    it "Number 1_015_423 has a 3 in it, the game says 'Fizz'" do
+      result = FizzBuzz.play(1_012_423)
 
       expect(result).to eql 'Fizz'
     end
 
-    it "Number has a 5 in it, the game says 'Buzz'" do
+    it "Number 1_484_695 has a 5 in it, the game says 'Buzz'" do
       result = FizzBuzz.play(1_484_695)
 
       expect(result).to eql 'Buzz'
     end
 
-    #2 test cases missing for when number has 3 in it but is also divisible between 5.
-    # Same switching numbers. Buth should return 'FizzBuzz'
+    it "Number 1_995_399_5533 has a 3 and a 5 in it, the game says 'FizzBuzz'" do
+      result = FizzBuzz.play(1_995_399_5533)
+
+      expect(result).to eql 'FizzBuzz'
+    end
+
   end
 end
