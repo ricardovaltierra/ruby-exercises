5ddabc986ed34727f894860564090a8a85d47e92
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-05-26 04:02:08 -0500

    RUBY DOJO - Module 1 - Testing with RSpec exercise_10

--- Module1/testing-with-rspec/exercise_10/lib/calc.rb
+++ Module1/testing-with-rspec/exercise_10/lib/calc.rb
@@ -1,31 +1,40 @@
 module Calc
   class << self
     def add(*args)
-      # your code here
+      args.reduce(:+)
     end
 
     def sub(a, b)
-      # your code here
+      a - b
     end
 
     def mul(*args)
-      # your code here
+      args.reduce(:*)
     end
 
     def div(a, b)
-      # your code here
+      a.to_f / b
     end
 
     def factors(n)
-      # your code here
+      (1..n).to_a.select do |i|
+        n % i == 0
+      end
     end
 
     def prime(n)
-      # your code here
+      return false if n.negative?
+      return false if n == 0 || n == 1
+      n_divisible = (1..n).to_a.select { |i| n % i == 0 }
+      n_divisible.length > 2 ? false : true
     end
 
     def primes_in_range(first, last)
-      # your code here
+      first -= first.negative? ? first - 2 : 0
+      (first..last).to_a.filter_map do |n|
+        n_divisible = (1..n).to_a.select { |i| n % i == 0 }
+        n unless n_divisible.length > 2
+      end
     end
   end
 end
