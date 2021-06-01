20e3b0254912f757ecef62809c658d7a4a5c2ffd
ricardovaltierra <ricardo_valtierra@outlook.com>
2021-06-01 16:10:15 -0500

    RUBY DOJO - Module 1 - stubs-and-mocks

--- Module1/stubs-and-mocks/spec/image_generator_spec.rb
+++ Module1/stubs-and-mocks/spec/image_generator_spec.rb
@@ -2,18 +2,41 @@ require 'rspec'
 require './lib/image_generator'
 
 RSpec.describe ImageGenerator do
+
+  before :each do
+    @image_generator = ImageGenerator.new(2500)
+  end
+
   describe '#initialize' do
     it 'exists' do
-      image_generator = ImageGenerator.new(2500)
-      expect(image_generator).to be_a ImageGenerator
+      expect(@image_generator).to be_a ImageGenerator
     end
 
     it 'has attributes' do
-      image_generator = ImageGenerator.new(2500)
-      expect(image_generator.max_image_size).to eq 2500
+      expect(@image_generator.max_image_size).to eq 2500
     end
 
     # Using stubs write the tests to confirm the functionality
     # that exists in ./lib/image_generator.rb
+  end
+
+  describe 'generate_images' do
+    it 'generates images' do
+      random_number = rand(2500)
+      images = (0..random_number).to_a.map { |num| "image#{num}" }
+
+      allow(@image_generator).to receive(:generate_images).and_return(images: images)
+    end
+  end
+
+  describe 'change_max_size' do
+    it 'changes max_image_size' do
+      num = 3
+
+      max_image_size = (num * rand(2500))
+
+      allow(@image_generator).to receive(:change_max_size).with(num).and_return(max_image_size: max_image_size)
+      allow(@image_generator).to receive(:max_image_size).and_return(max_image_size)
+    end
   end
 end
--- Module1/stubs-and-mocks/spec/user_spec.rb
+++ Module1/stubs-and-mocks/spec/user_spec.rb
@@ -3,22 +3,86 @@ require './lib/image_generator'
 require './lib/user'
 
 RSpec.describe User do
+
+  before :each do
+    @mock_image_generator = instance_double("ImageGenerator", max_image_size: 2)
+    @user = User.new("t@gmail.com", @mock_image_generator)
+  end
+
   describe '#initialize' do
     it 'exists' do
       # I pass a real image generator object - this is potentially slow and costly
       # Lets mock this object whenever possible
-      image = ImageGenerator.new(20)
-      user = User.new("t@gmail.com", image)
-      expect(user).to be_a User
+      expect(@user).to be_a User
     end
 
     it 'has attributes' do
       #I don't need an actual ImageGenerator object. I use a mock instead.
-      mock_image_generator = instance_double("Image", max_image_size: 45)
-      user = User.new("t@gmail.com", mock_image_generator)
+      # mock_image_generator = instance_double("Image", max_image_size: 45)
+      # user = User.new("t@gmail.com", @mock_image_generator)
 
-      expect(user.email).to eq "t@gmail.com"
-      expect(user.image_generator).to eq mock_image_generator
+      expect(@user.email).to eq "t@gmail.com"
+      expect(@user.image_generator).to eq @mock_image_generator
+      expect(@user.id).to be_between(1, 100000).inclusive
+    end
+  end
+
+  describe '#change_id_via_user_input' do
+    it 'returns :id from get_user_input method' do
+      allow(@user).to receive(:get_user_input).and_return 3
+
+      @user.change_id_via_user_input
+    
+      expect(@user.id).to eq 3
+    end
+  end
+
+  describe '#get_user_input' do
+    it 'should receive an input from console converted into integer' do 
+      @user.stub(:gets).and_return('3\n')
+      expect(@user.get_user_input).to eql  3
+    end
+  end
+
+  describe '#add_image_generators' do
+    it 'saves one image received as a parameter into @image_history\'s array' do 
+      my_image = "image1"
+
+      result = @user.add_image_generators my_image
+
+      expect(result).to eq [my_image]
+    end
+  end
+
+  describe '#create_images' do
+    it 'class #generate_images from @image_processor' do 
+     allow(@user.image_generator).to receive(:generate_images).and_return ["image1", "image2"]
+
+     result = @user.create_images
+
+     expect(result).to eq ["image1", "image2"]
+    end
+  end
+
+  describe '#assign_new_random_image' do
+    it 'assigns @profile_image a random image from #random_image, calling #add_image_history' do 
+      my_images = ["image1","image2"]  
+    
+      allow(@user.image_generator).to receive(:generate_images).and_return my_images
+  
+      @user.assign_new_random_image
+
+      expect(my_images).to include @user.random_image
+    end
+  end
+
+  describe '#random_image' do
+    it 'returns a random image from the @image_generator' do
+      my_images = ["image1","image2"]
+      allow(@user.image_generator).to receive(:generate_images).and_return(["image1","image2"])
+
+      # Ask for implementation where the explected result is contained inside 'my_images' array,
+      expect(my_images).to include @user.random_image
     end
   end
 end
